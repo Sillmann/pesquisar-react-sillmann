39271a148f8d15436278493bea38c475494446df
ssilman <ssilman@gmail.com>
2024-03-12 21:30:05 -0300

    atualizacoes

--- frontend/src/Listar/index.tsx
+++ frontend/src/Listar/index.tsx
@@ -1,48 +1,104 @@
-import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 import Praia1 from '../assets/praia1.jpeg'
 import Praia2 from '../assets/praia2.jpeg';
 import Praia3 from '../assets/praia3.jpeg';
 
-interface Props {
-  foto: string,
-  localizacao: string
-}
-
-function ListarItem(props: Props) {
-  return (
-
-      <div className="item">
-        <div className="foto">
-          <img src={props.foto} alt="praia"/>
-        </div>
-        <div className="localizacao">
-          {props.localizacao}</div>
-      </div>
-
-
-  )
-}
+// npm install --save-dev @types/react @types/react-dom
 
 function Listar() {
 
+  interface Props {
+    foto: string,
+    localizacao: string
+  }
   const [praias, setPraias] = useState([
-    {
-      foto:Praia1,
-      localizacao:"Porto de Galinhas"
-    },
-    {
-      foto:Praia2,
-      localizacao:"Carneiros"
-    },
-    {
-      foto:Praia3,
-      localizacao:"Natal"
-    }]);
+  {
+    foto:Praia1,
+    localizacao:"Porto de Galinhas"
+  },
+  {
+    foto:Praia2,
+    localizacao:"Carneiros"
+  },
+  {
+    foto:Praia3,
+    localizacao:"Natal"
+  }]);
 
-  return (
+
+  // useEffect(() => {
+  //   setPraias(getPraias());
+  // }, [])
+
+  function getPraias() {
+      return [
+        {
+          foto:Praia1,
+          localizacao:"Porto de Galinhas"
+        },
+        {
+          foto:Praia2,
+          localizacao:"Carneiros"
+        },
+        {
+          foto:Praia3,
+          localizacao:"Natal"
+        }
+    ]
+  }
+
+ 
+
+  const [descricao,setDescricao] = useState('');
+
+  const [verBotao, setVerBotao] = useState(true);
+  
+  function ListarItem(props: Props) {
+    return (
+  
+        <div className="item">
+          <div className="foto">
+            <img src={props.foto} alt="praia"/>
+          </div>
+          <div className="localizacao">
+            {props.localizacao}</div>
+        </div>
+  
+  
+    )
+  }
+
+  function IrBuscar() {
+    const results = praias.filter(item => item.localizacao.toLowerCase().indexOf(descricao.toLowerCase()) !== -1);
+    setPraias(results);
+    setVerBotao(!verBotao);
+
+  }
+
+  function IrLimpar() {
+
+    setPraias(getPraias());
+    // setPraias([
+    //   {
+    //     foto:Praia1,
+    //     localizacao:"Porto de Galinhas"
+    //   },
+    //   {
+    //     foto:Praia2,
+    //     localizacao:"Carneiros"
+    //   },
+    //   {
+    //     foto:Praia3,
+    //     localizacao:"Natal"
+    //   }]);
+    setVerBotao(!verBotao);
+  }
+  
+
+
+    return (
     // <div className="main">
     //   <ListarItem foto={Praia1} localizacao={"Porto de Galinhas"}/>
     //   <ListarItem foto={Praia2} localizacao={"Carneiros"}/>
@@ -50,13 +106,43 @@ function Listar() {
     // </div>  
 
 
-    <div className="main">
+    <div className="Main">
     
-          <form className="form">
-            <input className="input" 
-              placeholder="Pesquise sua praia de hoje"
-            />
-          </form>
+
+
+
+          <div className="busca">
+            
+            {verBotao && (
+              <input className="input" 
+                type="text"
+                placeholder="Pesquise sua praia de hoje"
+                value={descricao}    
+                onChange={e => setDescricao(e.target.value)}
+              />
+            )}               
+
+            {verBotao && (
+              <button 
+                type="button"
+                className="button" 
+                onClick={IrBuscar}
+                >Buscar</button>
+            )}   
+
+            {!verBotao && (
+              <button 
+                type="button"
+                className="button" 
+                onClick={IrLimpar}
+                >Nova Busca</button>
+            )}   
+
+
+          </div>
+
+
+          
 
           <div className="itens">
 
